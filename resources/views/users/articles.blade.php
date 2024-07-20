a1ad199e03a4120704ba0b43f979138be71ff2b9
reinskywalker <andre.reynaldi@ai.astra.co.id>
2024-07-20 21:02:52 +0700

    add article dummy

--- resources/views/users/articles.blade.php
+++ resources/views/users/articles.blade.php
@@ -16,12 +16,8 @@
                         <div class="flex flex-wrap -m-4 text-center">
                             <div class="p-4 md:w-1/4 sm:w-1/2 w-full ">
                                 <div class="border-2 border-gray-200 px-4 py-6 rounded-lg bg-white">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-                                        <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
-                                        <circle cx="9" cy="7" r="4"></circle>
-                                        <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
-                                    </svg>
-                                    <p class="leading-relaxed">Users</p>
+                                    <img src="{{ asset('images/dummy_image.jpg') }}" class="card-img-top img-fluid" alt="Dummy Image">
+                                    <p class="leading-relaxed">Migrasikan Infrastruktur IT Anda ke Cloud Anda dengan AWS dan Elitery</p>
                                 </div>
                             </div>
                         </div>
